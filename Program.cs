5d6813e39f4c1a3d54e41f06fa12cf6ed04b68d4
holdenprkr <holdenprkr@gmail.com>
2020-02-28 11:27:22 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            List<string> planetList = new List<string> () { "Mercury", "Mars" };
+
+            planetList.Add ("Jupiter");
+            planetList.Add ("Saturn");
+
+            List<string> lastTwoPlanets = new List<string> () { "Uranus", "Neptune" };
+
+            planetList.AddRange (lastTwoPlanets);
+
+            planetList.Insert (1, "Earth");
+            planetList.Insert (1, "Venus");
+
+            planetList.Add ("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange (0, 4);
+
+            planetList.Remove ("Pluto");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine (planet);
+            }
+
+            foreach (string planet in rockyPlanets)
+            {
+                Console.WriteLine (planet);
+            }
+
+            Random random = new Random ();
+            List<int> numbers = new List<int>
+            {
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+            };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if (numbers.Contains (i))
+                {
+                    Console.WriteLine ($"Numbers list contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine ($"Numbers list does not contain {i}");
+                }
+            }
+
+        }
+    }
+}
